ef7edad8f8ac6d84f6cf9f1cd8463e75a6eb7630
s-ono <s-ono@jokan.co.jp>
2023-03-16 15:12:40 +0900

    index削除

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,6 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  root "line_bot#index"
   post '/', to: 'line_bot#callback'
 
 end
