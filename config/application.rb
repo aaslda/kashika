7e502c18a7043ee628509fdacb036f838a3754bf
s-ono <s-ono@jokan.co.jp>
2023-03-16 15:18:47 +0900

    host削除

--- config/application.rb
+++ config/application.rb
@@ -18,7 +18,6 @@ module App
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
-    config.hosts << 'https://kahika.onrender.com'
 
   end
 
